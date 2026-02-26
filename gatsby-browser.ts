82a406c34a1fb2721ff6772a991c37d2c4fd6716
Dongjoon Lee <dongjoon.wow@gmail.com>
2026-02-26 18:20:24 -0500

    feat: configure Partytown for Google Analytics 4 and fix SPA route tracking

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,2 +1,18 @@
 // gatsby-browser.ts
 import './src/utils/fontawesome';
+import type { GatsbyBrowser } from 'gatsby';
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+    GA_ID?: string;
+  }
+}
+
+export const onRouteUpdate: GatsbyBrowser['onRouteUpdate'] = ({ location }) => {
+  if (typeof window.gtag === 'function' && window.GA_ID) {
+    window.gtag('config', window.GA_ID, {
+      page_path: location.pathname + location.search + location.hash,
+    });
+  }
+};
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import type { GatsbySSR } from "gatsby";
 import { Partytown } from "@qwik.dev/partytown/react";
 
-export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHtmlAttributes, setHeadComponents }) => {
+export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHtmlAttributes, setHeadComponents, setPreBodyComponents }) => {
   setHtmlAttributes({ lang: 'en' });
   
   const gaId = process.env.GA_ID;
+  const gtmId = process.env.GTAG_ID;
+
   if (gaId && gaId !== "undefined" && gaId !== "") {
     setHeadComponents([
-      <Partytown key="partytown" forward={['gtag', 'dataLayer.push']} />,
+      <Partytown 
+        key="partytown" 
+        forward={['gtag', 'dataLayer.push']} 
+        lib="/~partytown/"
+      />,
       <script
-        key="gtag-partytown"
+        key="gtag-js"
         type="text/partytown"
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
       />,
@@ -23,10 +29,30 @@ export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHtmlAttributes, set
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${gaId}', {
-              page_path: typeof location !== 'undefined' ? location.pathname + location.search + location.hash : undefined,
               anonymize_ip: true,
               cookie_expires: 0,
             });
+          `,
+        }}
+      />,
+      // We set the ID on the main thread so gatsby-browser.ts can access it
+      <script
+        key="gtag-id-main"
+        dangerouslySetInnerHTML={{
+          __html: `window.GA_ID = '${gaId}';`,
+        }}
+      />,
+    ]);
+  }
+
+  if (gtmId && gtmId !== "undefined" && gtmId !== "") {
+    setPreBodyComponents([
+      <noscript
+        key="gtm-noscript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0"
+                style="display:none;visibility:hidden"></iframe>
           `,
         }}
       />,
