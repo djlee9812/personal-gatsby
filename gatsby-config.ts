cc5354d4e3796eee40c8e04338899a76626ca363
Dongjoon Lee <dongjoon.wow@gmail.com>
2026-03-04 21:57:40 -0500

    refactor: improve type safety and structure in projects.ts

---
+++ gatsby-config.ts
@@ -0,0 +1,71 @@
+import type { GatsbyConfig } from "gatsby";
+import "dotenv/config";
+
+const config: GatsbyConfig = {
+  graphqlTypegen: true,
+  siteMetadata: {
+    title: `Dongjoon Lee's Personal Website`,
+    siteUrl: `https://www.dongjoonlee.com`,
+    description: `Dongjoon Lee's Personal Website`,
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "blogs",
+        path: `${__dirname}/blogs/`,
+      },
+      __key: "blogs",
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        resolveEnv: () => process.env.CONTEXT || process.env.NODE_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: "*", allow: "/" }],
+          },
+          "branch-deploy": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+          "deploy-preview": {
+            policy: [{ userAgent: "*", disallow: ["/"] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
+    },
+    "gatsby-plugin-smoothscroll",
+    {
+      resolve: "gatsby-source-cloudinary",
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: "image",
+        maxResults: 500,
+        context: true,
+        tags: true,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-cloudinary",
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        transformTypes: ["CloudinaryMedia"],
+      },
+    },
+  ],
+};
+
+export default config;
