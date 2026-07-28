56654005edb8c7b3dd2cc29f715a8c913132f0b2
Dongjoon Lee <dongjoon.wow@gmail.com>
2026-07-27 21:20:04 -0400

    load fontawesome to prevent icon resize on load

--- src/utils/fontawesome.ts
+++ src/utils/fontawesome.ts
@@ -1,5 +1,6 @@
 // src/utils/fontawesome.ts
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import {
   faArrowLeft,
   faArrowRight,
@@ -9,6 +10,10 @@ import {
   faPlane,
 } from '@fortawesome/free-solid-svg-icons';
 import { faApple, faGithub, faInstagram, faLinkedin, faGoodreads } from '@fortawesome/free-brands-svg-icons';
+
+// Ship FA sizing CSS with the SSR HTML. Default autoAddCss injects styles only
+// after client JS runs, so icons briefly render at raw SVG viewBox size.
+config.autoAddCss = false;
 
 library.add(
   faArrowLeft,
